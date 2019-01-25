0704a266c02bc7a2d4a076a142dc722e2470a29e
hernando <hernando.z@outlook.com>
2019-01-25 06:06:50 +0000

    initial

---
+++ HaveYourSay/Views/FormEntry.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HaveYourSay.Views
+{
+    public partial class FormEntry : ContentPage
+    {
+        public FormEntry()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HaveYourSay/Views/ScanView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace HaveYourSay.Views
+{
+    public partial class ScanView : ContentPage
+    {
+        public ScanView()
+        {
+            InitializeComponent();
+        }
+
+        async void Handle_ClickedAsync(object sender, System.EventArgs e)
+        {
+            var scanPage = new ZXingScannerPage();
+            // Navigate to our scanner page
+            await Navigation.PushAsync(scanPage);
+
+            scanPage.OnScanResult += (result) =>
+            {
+                // Stop scanning
+                scanPage.IsScanning = false;
+
+                // Pop the page and show the result
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await Navigation.PopAsync();
+                    await DisplayAlert("Scanned Barcode", result.Text, "OK");
+                });
+            };
+        }
+    }
+}
